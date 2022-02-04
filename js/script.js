d3f929df5c302c56927bcba73cdf5dd85f4f9c27
Victor Matos <antoniovitomatos@gmail.com>
2022-02-04 10:40:49 -0300

    virar a carta e mostrar a outra face

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
-distribuirCartas();
+
+let allCards = [
+    "bobrossparrot.gif",
+    "explodyparrot.gif",
+    "fiestaparrot.gif",
+    "metalparrot.gif",
+    "revertitparrot.gif",
+    "tripletsparrot.gif",
+    "unicornparrot.gif"
+];
 
 function distribuirCartas(){
-   
-    let quantidade = prompt("Dgite um valor par entre 4 e 14 paracomeçarmos!");
+    let quantidade = prompt("Dgite um valor par entre 4 e 14 para começarmos!");
     while(quantidade%2 !== 0 || quantidade < 4 || quantidade > 14){
         if(quantidade%2 !== 0){
             quantidade = prompt("Eu disse par!");
@@ -14,16 +22,19 @@ function distribuirCartas(){
 
 
     for(let i = 0; i< quantidade; i++){
+        console.log(allCards[i]);
         const conteudo = document.querySelector('.game');
         conteudo.innerHTML += `
-        <div class="card ">
-            <div class="front-face face">
-                <img src="/content/front.png" alt="">
-            </div>
-            <div class="back-face face">
-                Verso
-            </div>
+        <div class="card" onclick= "cartaSelecionada(this)">
+        <img class="front-face remove" src="content/${allCards[i]}"/>
+        <img class="back-face" src="content/front.png"/>
         </div>`;
     }
     
 }
+function cartaSelecionada(element){
+    element.classList.add("flip");
+    element.querySelector(".front-face").classList.toggle("remove");
+    element.querySelector(".back-face").classList.toggle("remove");
+}
+distribuirCartas();
