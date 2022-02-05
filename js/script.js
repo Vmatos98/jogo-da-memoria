d65c48a22cfee36d91ddeda89cf1fa85212076fc
Victor Matos <antoniovitomatos@gmail.com>
2022-02-05 13:23:49 -0300

    Contagem de jogadas e pontos, remover onclick de cartas combinadas

--- js/script.js
+++ js/script.js
@@ -11,8 +11,10 @@ const allCards = [
 const gameCards = [];
 let firstCard, secondCard;
 let lockgame = false;
+let count = 0, point = 0;
+let amount;
 function distribuirCartas(){
-    let amount = prompt("Dgite um valor par entre 4 e 14 para começarmos!");
+    amount = prompt("Dgite um valor par entre 4 e 14 para começarmos!");
     while(amount%2 !== 0 || amount < 4 || amount > 14){
         if(amount%2 !== 0){
             amount = prompt("Eu disse par!");
@@ -50,7 +52,9 @@ function selectedCard(element){
     element.classList.add("flip");
     element.querySelector(".front-face").classList.remove("remove");
     element.querySelector(".back-face").classList.add("remove");
-
+    count++;
+    count = parseInt(count);
+    console.log(count);
     if(!firstCard){
         firstCard = element;
         return false;
@@ -61,16 +65,28 @@ function selectedCard(element){
 }
 function checkMatch() {
     if(firstCard.dataset.card === secondCard.dataset.card){
+        firstCard.removeAttribute("onclick");
+        secondCard.removeAttribute("onclick");
         lockgame = false;
         firstCard = null;
         secondCard = null;
+        point++;
+        point= parseInt(point);
+        console.log(point);
+        
     }
     else{
-        setTimeout(reset, 1000);
+        setTimeout(resetCard, 1000);
+    }
+    if(point === amount/2){
+       const endGame = prompt(`Você ganhou em ${count} rodadas\n Deseja jogar novamente? (S/N)`);
+       if(endGame === "s"||endGame === "S"){
+        location.reload();
+       }else{return false;}
     }
     
 }
-function reset(){
+function resetCard(){
     firstCard.classList.remove("flip");
     firstCard.querySelector(".front-face").classList.add("remove");
     firstCard.querySelector(".back-face").classList.remove("remove");
@@ -80,5 +96,8 @@ function reset(){
     lockgame = false;
     firstCard = null;
     secondCard = null;
+}
+function resetGame(){
+    
 }
 distribuirCartas();
